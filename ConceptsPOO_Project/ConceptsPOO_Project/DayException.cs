b5e2a17116c8ab908de7a5d843dd4ca5216a19c3
Pedro Pinero <penriques1985@gmail.com>
2022-10-20 00:26:11 -0400

    Finish Date class

---
+++ ConceptsPOO_Project/ConceptsPOO_Project/DayException.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+[Serializable]
+internal class DayException : Exception
+{
+    public DayException()
+    {
+    }
+
+    public DayException(string? message) : base(message)
+    {
+    }
+
+    public DayException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected DayException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ ConceptsPOO_Project/ConceptsPOO_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+
+//Date date1 = new Date(2022, 2, 11);
+//Console.WriteLine(date1);
+
+try
+{
+	Console.WriteLine(new Date(2024, 1, 31));
+	Console.WriteLine(new Date(2022, 10, 1));
+	Console.WriteLine(new Date(2022, 2, 19));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
+
+Console.Read();
+
